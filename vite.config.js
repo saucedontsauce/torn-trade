8856b22b1af741b715b4451e48a7a9cde8c9aa0d
saucedontsauce <98565306+saucedontsauce@users.noreply.github.com>
2025-10-17 15:10:17 +0100

    build

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss()],
   base: mode === 'production' ? '/torn-trade/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  }
-})
+  },
+}))
